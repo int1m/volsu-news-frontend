db04dbc548e3af88159fcc0ba85027d100520de9
int1m <maxmonsta@gmail.com>
2022-02-24 03:26:51 +0300

    feat(utilities.test): add tests

--- src/helpers/utilities.test.ts
+++ src/helpers/utilities.test.ts
@@ -2,4 +2,7 @@ import { cropNumber } from '@/helpers/utilities';
 
 test('cropNumber', () => {
   expect(cropNumber(341)).toBe('341');
+  expect(cropNumber(999)).toBe('999');
+  expect(cropNumber(1000)).toBe('1K');
+  expect(cropNumber(1120)).toBe('1.1K');
 });
